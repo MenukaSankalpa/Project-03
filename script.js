6bfaaff823fc9d9057cc0e12e5335ea58371ed97
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-04 17:31:34 +0530

    update again

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let inputSlider = document.getElementById("inputSlider")
-let sliderValue = document.getElementById("sliderValue")
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
 
 
 //showing input slider value 
@@ -7,3 +13,9 @@ sliderValue.textContent = inputSlider.value;
 inputSlider.addEventListener('input', ()=>{
     sliderValue.textContent = inputSlider.value;
 }); 
+
+genBtn.addEventListener('click', ()=>{
+    passBsx.value = generatePassword();
+});
+
+//Function to generate password 
